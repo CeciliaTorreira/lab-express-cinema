f35a30ca4a277f1559c788b4a0669488d40abf03
Cecilia Torreira <celia.umbreon@gmail.com>
2023-05-07 10:55:39 +0100

    Las películas se muestran correctamente en /movies  (Iteracion 3)

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,30 @@
 const express = require('express');
 const router = express.Router();
 
+const Movie = require('../models/Movie.model');
+
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+//* GET "/movies" => Lista de películas
+
+router.get("/movies", (req, res, next) => {
+ Movie.find()
+ .then((response) => {
+    console.log(response);
+
+    res.render("../views/movies.hbs", {
+        allMovies: response
+    });
+ })
+
+.catch((error)=>{
+    next(error);
+   })
+});
+
+
+
+
 
 module.exports = router;
